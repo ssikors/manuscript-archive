5b0af983b91e2b12503a1a241a4b01b067b2c9c3
ssikors <stanislaw.s.2000@gmail.com>
2025-05-16 23:22:08 +0200

    changed .NET version to include swagger

--- ManuscriptApi/Controllers/WeatherForecastController.cs
+++ ManuscriptApi/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace ManuscriptArchiveAPI.Controllers;
+namespace ManuscriptApi.Controllers;
 
 [ApiController]
 [Route("[controller]")]
--- ManuscriptApi/WeatherForecast.cs
+++ ManuscriptApi/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace ManuscriptArchiveAPI
+namespace ManuscriptApi
 {
     public class WeatherForecast
     {
